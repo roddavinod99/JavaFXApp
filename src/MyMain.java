2e5dff96d00e03bbac92088e8c6af3c956c3ac75
Vinod Kumar Rodda <76143573+roddavinod99@users.noreply.github.com>
2022-04-19 17:22:33 +0530

    add the menu Bar to the application

--- src/MyMain.java
+++ src/MyMain.java
@@ -1,13 +1,18 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MyMain extends Application
 {
+
 	public static void main(String[] args)
 	{
+		System.out.println("main");
 		launch(args);
 	}
 
@@ -23,8 +28,11 @@ public class MyMain extends Application
 		System.out.println("start");
 		FXMLLoader loader = new
 				FXMLLoader(getClass().getResource("app_layout.fxml"));
-		Pane rootNode = loader.load();
+		VBox rootNode = loader.load();
 
+		MenuBar menuBar=  createMenu();
+		rootNode.getChildren().addAll(menuBar);
+		// this atatement will add the menu bar in the VBox
 		Scene scene = new Scene(rootNode, 300, 275);
 
 		primaryStage.setScene(scene);
@@ -32,6 +40,16 @@ public class MyMain extends Application
 		primaryStage.show();
 	}
 
+	private MenuBar createMenu() {
+
+		Menu fileMenu = new Menu("File");
+
+		Menu helpMenu = new Menu("Help");
+
+		MenuBar menuBar= new MenuBar();
+		menuBar.getMenus().addAll(fileMenu, helpMenu);
+		return menuBar;
+	}
 	@Override
 	public void stop() throws Exception {
 		System.out.println("Stop");
