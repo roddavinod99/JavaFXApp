7cc1e485dc92ed526148b6824579224d21aa31b2
Vinod Kumar Rodda <76143573+roddavinod99@users.noreply.github.com>
2022-04-19 21:14:03 +0530

    customising the click event on Dialog

--- src/MyMain.java
+++ src/MyMain.java
@@ -6,6 +6,8 @@ import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.Optional;
+
 public class MyMain extends Application
 {
 
@@ -75,7 +77,19 @@ public class MyMain extends Application
 			alertDialog.setTitle("My First Desktop App");
 			alertDialog.setHeaderText("Learning JavaFX");
 			alertDialog.setContentText("I am Just a beginner but soon I will be a pro and start developing awesome games.");
-			alertDialog.show();
+			ButtonType yesBtn= new ButtonType("yes");
+			ButtonType noBtn = new ButtonType("No");
+
+			alertDialog.getButtonTypes().setAll(yesBtn,noBtn);
+			Optional<ButtonType> clickBtn = alertDialog.showAndWait();
+
+			if (clickBtn.isPresent() && clickBtn.get() == yesBtn){
+				System.out.println("Yes button was clicked");
+			}
+
+			if (clickBtn.isPresent() && clickBtn.get() == noBtn){
+				System.out.println("No button was clicked");
+			}
 		}
 
 	@Override
